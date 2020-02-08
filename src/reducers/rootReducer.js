414d311151ec27fd7e270107d6fe5e8881d488e7
LawsonUK <chrislawsonuk@hotmail.com>
2020-02-08 20:08:16 +0000

    redux actions

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,8 +1,21 @@
 const InitState = {
-  posts: []
+  posts: [
+    { id: "1", title: "Title 1", body: "Body text 1" },
+    { id: "2", title: "Title 2", body: "Body text 2" },
+    { id: "3", title: "Title 3", body: "Body text 3" }
+  ]
 }
 
 const rootReducer = (state = InitState, action) => {
+  if (action.type === "DELETE_POST") {
+    let newPosts = state.posts.filter(post => {
+      return post.id !== action.id
+    })
+    return {
+      ...state,
+      posts: newPosts
+    }
+  }
   return state
 }
 
